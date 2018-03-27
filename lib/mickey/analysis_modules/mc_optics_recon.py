a5658b65ee64fad8283c2a1467431dd5097229e6
Christopher Hunt <christopher.hunt08@imperial.ac.uk>
2018-03-27 15:05:08 +0100

    Restructed cuts location in mickey lib

---
+++ lib/mickey/analysis_modules/mc_optics_recon.py
@@ -0,0 +1,41 @@
+
+from _analysis_base import Analysis_Base
+from analysis import inspectors
+
+import ROOT
+import math
+import array
+
+
+class MCTruthOpticsRecon(Analysis_Base) :
+
+  def __init__(self) :
+    Analysis_Base.__init__(self, "mc_trueth_optics_reconstruction")
+
+    self.__inspectors = []
+
+
+  def analyse_event(self, analysis_event, weight=1.0) :
+    pass
+
+
+  def _get_plots(self, plot_dict) :
+    pass
+
+
+  def _get_data(self, data_dict) :
+    pass
+
+
+  def configure_arguments(self, parser) :
+#    parser.add_argument( 
+    pass
+
+
+  def parse_arguments(self, namespace) :
+    pass
+
+
+  def conclude(self) :
+    pass
+
---
+++ lib/mickey/cuts_modules/__init__.py
@@ -0,0 +1,10 @@
+
+from banana_cut import BananaPlot
+from diffuser_cut import DiffuserAperture
+from scifi_cuts import SciFiUpstreamChisqNDF
+from scifi_cuts import SciFiUpstreamPt
+from scifi_cuts import SciFiRefitStatus
+from scifi_momentum_cut import SciFiUpstreamMomentum
+from tof_cuts import TOF01Spacepoints
+from tof_cuts import TOF01Time
+
